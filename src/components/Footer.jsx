2e855263afa134aba8fccef6b9f03e6d6f2ca4e4
vasujainjain <jainvasu552004@gmail.com>
2025-08-16 00:38:25 +0530

    Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="w-full py-4 mt-8 bg-[#232936] text-center text-gray-400 text-sm rounded-t-lg shadow-inner">
-      © {new Date().getFullYear()} Nishant Kumar Dwivedi. All rights reserved.
+      © {new Date().getFullYear()} Vasu Jain. All rights reserved
     </footer>
   );
 }
